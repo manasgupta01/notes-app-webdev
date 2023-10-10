7d0e1d1e22c8dee070c0980f5c5de6ac0579072f
manasgupta01 <manasgupta7624@gmail.com>
2023-10-10 14:04:08 +0530

    working project

---
+++ routes/userRouter.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+const useCtrl = require('../controllers/userCtrl')
+const auth = require('../middleware/auth')
+
+//Register User
+router.post('/register',useCtrl.registerUser)
+
+// Login user
+router.post('/login',useCtrl.loginUser)
+
+// verify token
+router.get('/verify', useCtrl.VerifiedToken)
+
+
+module.exports = router
